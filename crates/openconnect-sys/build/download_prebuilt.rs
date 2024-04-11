78403e4f56a6f0e56d6c7e1036c7654533de0791
hlhr202 <hlhr202@163.com>
2024-04-11 11:55:10 +0800

    build: prebuilt for aarch64 mac

--- crates/openconnect-sys/build/download_prebuilt.rs
+++ crates/openconnect-sys/build/download_prebuilt.rs
@@ -1,9 +1,17 @@
 use std::path::PathBuf;
 
+#[cfg(target_os = "macos")]
+#[cfg(target_arch = "aarch64")]
+const URL: &str = "https://master.dl.sourceforge.net/project/openconnect-prebuilt/prebuilt-openconnect-aarch64-apple-darwin.zip?viasf=1";
+
+#[cfg(target_os = "windows")]
+#[cfg(target_arch = "x86_64")]
+#[cfg(target_env = "gnu")]
+const URL: &str = "https://master.dl.sourceforge.net/project/openconnect-prebuilt/prebuilt-openconnect-msys.zip?viasf=1";
+
 pub fn download_from_sourceforge(outdir: PathBuf) {
-    let url = "https://master.dl.sourceforge.net/project/openconnect-prebuilt/prebuilt-openconnect-msys.zip?viasf=1";
-    let output = outdir.join("prebuilt-openconnect-msys.zip");
-    let mut response = reqwest::blocking::get(url).unwrap();
+    let output = outdir.join("openconnect.zip");
+    let mut response = reqwest::blocking::get(URL).unwrap();
     let mut file = std::fs::File::create(&output).unwrap();
     std::io::copy(&mut response, &mut file).unwrap();
 
